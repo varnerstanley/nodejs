dd83eae7f0fcef1ba9b17711d06839f4b7f33dc3
Stanley Varner <stanmanjr28@gmail.com>
2019-03-12 14:32:24 -0600

    trying again

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(express.static("public"));
 
@@ -52,7 +53,13 @@ app.get("/rates", getRates);
   console.log("I made it this far");
 }
 
-
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 // app.listen(3000, function() {
 //   console.log("Server started on 3000");
